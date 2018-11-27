6296a39bbc5e2f10ae2bf2f443f6143f5a6bd1bf
Alexander Koc <alexander.koc@slu.se>
2018-11-27 13:42:30 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 # Simple Install Script
 
-executable_install_path="$HOME/.local/bin/note"
-# install_dir="$HOME/.local/note-keeper"
+install_dir="$HOME/.local/bin"
+executable_install_path="$install_dir/note"
+
 
 mkdir -p "$install_dir"
 
-curl -o "$install_dir/note" https://raw.githubusercontent.com/alkc/note-keeper/thesis-notes/note
-chmod +x "$install_path" 
+curl -o "$executable_install_path" https://raw.githubusercontent.com/alkc/note-keeper/thesis-notes/note
+chmod +x "$executable_install_path" 
 
-ln -s "$install_dir/note" "$executable_install_path"
+ln -s "$executable_install_path" "$HOME/.local/bin/tl"
 
-echo "Note Keeper installed ($install_path)."
+echo "Note Keeper installed ($executable_install_path)."
 echo "Don't forget to set export THESIS_LOG_DIR in yr .bashrc!"
