cddef04acd9ab1de7dad7129ab9610d6a6461f73
Alexander Koc <alexander.koc@slu.se>
2018-11-27 14:42:39 +0100

    Changed name of note executable to tl (thesis-log)

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
-#!/bin/sh
+\1;5202;0c#!/bin/sh
 
 # Simple Install Script
 
 install_dir="$HOME/.local/bin"
-executable_install_path="$install_dir/note"
+executable_install_path="$install_dir/tl"
 
 
 mkdir -p "$install_dir"
 
 curl -o "$executable_install_path" https://raw.githubusercontent.com/alkc/note-keeper/thesis-notes/note
 chmod +x "$executable_install_path" 
-
-ln -s "$executable_install_path" "$HOME/.local/bin/tl"
 
 echo "Note Keeper installed ($executable_install_path)."
 echo "Don't forget to set export THESIS_LOG_DIR in yr .bashrc!"
